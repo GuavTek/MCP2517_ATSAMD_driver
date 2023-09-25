49a51fa46f371ac482dfb8d746f479dffec1ceac
GuavTek <36814224+GuavTek@users.noreply.github.com>
2023-09-25 21:31:25 +0200

    Add temporary RP2040 files

---
+++ SPI_RP2040.cpp
@@ -0,0 +1,9 @@
+/*
+ * SPI_RP2040.cpp
+ *
+ * Created: 25/09/2023
+ *  Author: GuavTek
+ */ 
+
+#include "SPI_RP2040.h"
+
---
+++ SPI_RP2040.h
@@ -0,0 +1,27 @@
+﻿/*
+ * SPI_RP2040.h
+ *
+ * Created: 25/09/2023
+ *  Author: GuavTek
+ */ 
+
+
+#ifndef SPI_RP2040_H_
+#define SPI_RP2040_H_
+#include "communication_base.h"
+
+class SPI_RP2040_C : public communication_base_c
+{
+	public:
+		inline void Handler();
+		SPI_RP2040_C();
+	protected:
+		char msgBuff[32];
+};
+
+// SPI interrupt handler
+inline void SPI_RP2040_C::Handler(){
+
+}
+
+#endif /* SPI_SAMD_H_ */
